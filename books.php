75522ba2effe33bd644cf811124ed53f2a4caf94
AgentKid <agentkid20@gmail.com>
2012-05-30 16:27:09 -0400

    Fixed misc stuff

--- books.php
+++ books.php
@@ -1,6 +1,8 @@
 <?php
-// Fetch the book number
-$booknumber = $_POST['args[2]'];
+// Fetch variables
+$args = $_POST[args];
+$name = $_POST[player];
+$booknumber = $args[2];
 
 include("includes/mysql.php");
 include("includes/books.php");
@@ -13,16 +15,19 @@ $divisor_for_seconds = $divisor_for_minutes % 60;
 $seconds = ceil($divisor_for_seconds);
 
 // Get completed books
-mysql_query("SELECT * FROM bookscompleted WHERE name='$name'");
+$completed = array();
+$completedquery = mysql_query("SELECT * FROM bookscompleted WHERE name='$name'");
+while($completedrow = mysql_fetch_array($completed)){
+	array_push($completed, $completedrow[book]);
+}
+if($_REQUEST[debug]==true){ var_dump($completed); }
 
+// MySQL table "books": id (auto-inc, integer) | name (text) | booknum (int) | timestart (time)
 // Is a book currently being read?
-$currentbooksql = mysql_query("SELECT * FROM books WHERE name='$name'") 
-or die(mysql_error());
-$currentbookrow = mysql_fetch_array( $currentbooksql );
-$currentbookbeingread = $currentbookrow['$name'];
-if ($currentbookbeingread == $name) {
+$currentrow = mysql_fetch_array( mysql_query("SELECT * FROM books WHERE name='$name'") );
+if ($currentrow[name] == $name) {
 	$type = "readtime";
-	$oldbookid = $currentbookrow['booknumber'];
+	$oldbookid = $currentrow[booknum];
 }
 $time = time();
 // Set type
@@ -35,7 +40,7 @@ if ($alreadyread != "true" && $stuff == "true") {
 	$type = "queststart";
 	if ( $questset == 1) {
 		mysql_query("INSERT INTO permissions (id, name, type, permission) VALUES ('NULL', '$name', '1', 'book.completed.1.8')");
-		mysql_query("INSERT INTO books (id, name, booknumber, bookstart) VALUES('NULL', '$name', '$booknumber', '$time')");
+		mysql_query("INSERT INTO books (id, name, booknumber, bookstart) VALUES ('NULL', '$name', '$booknumber', '$time')");
         }
 }
 		
