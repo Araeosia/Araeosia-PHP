a8b351446477859fe0c82ee280885dea540e6241
AgentKid <agentkid20@gmail.com>
2012-06-07 08:33:24 -0400

    Added quest message along with names of quests.

--- motd.php
+++ motd.php
@@ -17,6 +17,39 @@ switch($world){
 		$worldnamef = "The Tutorial";
 		break;
 }
+$query = mysql_fetch_array(mysql_query("SELECT * FROM permissions WHERE permission LIKE quest.current.%.%.%"))
+$quest = $query[permission];
+$questexploded=explode($quest, '.');
+switch($questexploded[2]){
+	case "tutorial":
+		$questname = "The Tutorial";
+		break;
+	case "nomoregooddays":
+		$questname = "No More Good Days, Part ".$questexploded[3];
+		break;
+	case "ships":
+		$questname = "Ship Travel, Part ".$questexploded[3];
+		break;
+	case "caverncatastrophe":
+		$questname = "Cavern Catastrophe, Part ".$questexploded[3];
+		break;
+	case "coalcrisis":
+		$questname = "Coal Crisis, Part ".$questexploded[3];
+		break;
+	case "archaeologist":
+		$questname = "The Archaeologist, Part ".$questexploded[3];
+		break;
+	case "familytrouble":
+		$questname = "Family Troubles, Part ".$questexploded[3];
+		break;
+	case "fruity":
+		$questname = "A Fruity Conundrum";
+		break;
+	default:
+		$questname = "UNKNOWN QUEST NAME";
+		break;
+}
 if(in_array("Tutorial", $groups)){ echo "§2Welcome to Araeosia, §b".$name."\n/Command/ExecuteBukkitCommand:ch join Tutorial;\n/Command/ExecuteBukkitCommand:ch leave Araeosia;\n§eYou can skip this tutorial using §9/tutorialskip§f.\n§cUntil you finish the tutorial, your chat is muted.\n§eTo begin the tutorial, right click on §6Gordon_Cassidy.\n§7There will be more useful information here after the tutorial.";}
 if(in_array("1", $groups)){ echo "§3You currently have §6$" . number_format($iconomyvalue) . " §3dollars.\n"; }
+if(isset($quest)){ echo "§2Your current quest is §7".$questname.".\n"; }
 ?>
