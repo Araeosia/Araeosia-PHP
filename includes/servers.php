b94e6168d18f124734df424194acb5fce1bfea8e
AgentKid <agentkid20@gmail.com>
2012-06-09 00:13:50 -0400

    Added Araeosia2 server A.

--- includes/servers.php
+++ includes/servers.php
@@ -1,11 +1,11 @@
 <?php
 // This file contains the IP addresses and ports for each of Araeosia's servers, viewed from inside Araeosia's local network. They are not accessible from outside the network, so don't even bother ;) The $ports array also has subarrays for Websend and mcqueries. This file is used for things such as messaging players from PHP and querying each server for players.
-$servers = array('RPG', 'Freebuild', 'Modded', 'Sandbox');
-$ips = array( 'RPG' => '192.168.5.106', 'Freebuild' => '192.168.5.104', 'Modded' => '192.168.5.106', 'Sandbox' => '192.168.5.106');
+$servers = array('RPG', 'Freebuild', 'Modded', 'Sandbox', '2');
+$ips = array( 'RPG' => '192.168.5.106', 'Freebuild' => '192.168.5.104', 'Modded' => '192.168.5.106', 'Sandbox' => '192.168.5.106', 'A2' => '192.168.5.106');
 $ports = array(
-	'mc' => array('RPG' => 25566, 'Freebuild' => 25565, 'Modded' => 25567, 'Sandbox' => 25569),
-	'websend' => array('RPG' => 4445, 'Freebuild' => 4445, 'Modded' => 4446, 'Sandbox' => 4447),
-	'jsonapi' => array('RPG' => 20059, 'Freebuild' => 20059, 'Modded' => 20061, 'Sandbox' => 20063),
-	'mysql' => array('RPG' => 3306, 'Freebuild' => 3306, 'Modded' => NULL, 'Sandbox' => 3306)
+	'mc' => array('RPG' => 25566, 'Freebuild' => 25565, 'Modded' => 25567, 'Sandbox' => 25569, 'A2' => 25570),
+	'websend' => array('RPG' => 4445, 'Freebuild' => 4445, 'Modded' => 4446, 'Sandbox' => 4447, 'A2' => '4448'),
+	'jsonapi' => array('RPG' => 20059, 'Freebuild' => 20059, 'Modded' => 20061, 'Sandbox' => 20063, 'A2' => 20065),
+	'mysql' => array('RPG' => 3306, 'Freebuild' => 3306, 'Modded' => NULL, 'Sandbox' => 3306, 'A2' => 3306)
 );
 ?>
