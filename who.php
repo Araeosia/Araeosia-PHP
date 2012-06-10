6d9be10c9559b7013cd339e44ad93726a2c5fbb8
AgentKid <agentkid20@gmail.com>
2012-06-10 02:44:37 -0400

    Cleaner code, removed some warnings, made it easier to do last second changes, added console cleanup.

--- who.php
+++ who.php
@@ -1,14 +1,15 @@
 <?php
 // This file handles the /who command used on all Araeosia servers.
 // Fetch variables
-$name=$_POST[player];
+$name=$_POST['player'];
+$output = null;
 include('includes/functions.php');
 include('includes/servers.php');
 include('includes/staff.php');
 foreach($servers as $server){
 	$Query = new MinecraftQuery();
 	try{
-		$Query->Connect( $ips[$server], $ports[mc][$server], 1 );
+		$Query->Connect( $ips[$server], $ports['mc'][$server], 1 );
 		$players = $Query->GetPlayers();
 	}catch(MinecraftQueryException $e){
 		// Only reason this query would fail is if the server is down. Report it
@@ -17,14 +18,16 @@ foreach($servers as $server){
 	// Now we have a populated (or empty) array of players, lets handle it.
 		$playersfinal = array();
 		foreach($players as $player){
-			if(in_array($player,$staffranks[admin])){$player="§4".$player;}elseif(in_array($player,$staffranks[moderator])){$player = "§a".$player;}else{$player = "§b".$player; }
+			if(in_array($player,$staffranks['admin'])){$player="§4".$player;}elseif(in_array($player,$staffranks['moderator'])){$player = "§a".$player;}else{$player = "§b".$player; }
 			array_push($playersfinal, $player);
 		}
-		echo "§c------- §bAraeosia ".$server." §c-------\n";
+		$output = $output."§c------- §bAraeosia ".$server." §c-------\n";
 	if(count($playersfinal)!=0 && $playersfinal!=false){
-		echo "§aOnline§f: ".implode('§f, ', $playersfinal)."\n";
+		$output = $output."§aOnline§f: ".implode('§f, ', $playersfinal)."\n";
 	}else{
-		echo "No online players!\n";
+		$output = $output."No online players!\n";
 	}
 }
+if($name=='console'){$output = str_replace(array('§1', '§2', '§3', '§4', '§5', '§6', '§7', '§8', '§9', '§0', '§a', '§b', '§c', '§d', '§e', '§f'), '', $output);}
+echo $output;
 ?>
