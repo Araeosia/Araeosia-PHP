c597a9a56726be869f46c8d04ad1d3ec5a3c9614
AgentKid <agentkid20@gmail.com>
2012-06-13 00:34:19 -0400

    Apparently cron doesn't like relative links :(

--- cronjobs/5minutely.php
+++ cronjobs/5minutely.php
@@ -1,9 +1,9 @@
 <?php
 // This file is executed by cron every 5 minutes. It handles things such as message timed message sending.
-include('includes/passwords.php');
-include('includes/servers.php');
-include('includes/mysql.php');
-include('includes/functions.php');
+include('/home/agentkid/web/includes/passwords.php');
+include('/home/agentkid/web/includes/servers.php');
+include('/home/agentkid/web/includes/mysql.php');
+include('/home/agentkid/web/includes/functions.php');
 $timestamp = "[".date('m-d-y H:i:s', time())."] ";
 $log = $timestamp."Attempting to cycle through ".implode($servers, ', ').".\n";
 $msgs = array( "§4[S] §aCheck out the forums at §bforums.araeosia.com§a!", "§bServer maintenance happens nightly around 1AM-2AM EST", "§bThe world map for Araeosia RPG is available at map.araeosia.com", "§4[S] §bYou can disable these messages with §c/optout§b!", "§4[S] §bAraeosia is accepting staff applications on the forums!", "§bAraeosia RPG is currently in beta testing! Help us find bugs!", "§bAraeosia has adopted a No-Mercy policy. §e/nomercy §bfor info.");
