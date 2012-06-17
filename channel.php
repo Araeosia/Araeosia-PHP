cd90393bb10707cd31efa1642b552d024eb034b3
AgentKid <agentkid20@gmail.com>
2012-06-17 02:56:08 -0400

    Base framework.

--- channel.php
+++ channel.php
@@ -1,4 +1,21 @@
 <?php
 $name = $_POST['player'];
 if($name!='AgentKid'){die('§4This is still a work-in-progress.'); }
+include('includes/mysql.php');
+include('includes/functions.php');
+include('includes/passwords.php');
+$args = $_POST['args'];
+switch($args[1]){
+	case "help":
+		break;
+	case "enter":
+	case "join":
+		break;
+	case "quit":
+	case "exit":
+	case "leave":
+		break;
+	case "list":
+		break;
+}
 ?>
