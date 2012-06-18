97e396d51748608d7dbd14ba2c549b034ed44c0d
AgentKid <agentkid20@gmail.com>
2012-06-17 20:05:41 -0400

    Forgot anoki/kidwulf! :O

--- includes/staff.php
+++ includes/staff.php
@@ -1,8 +1,8 @@
 <?php
 // This file contains the listing for the staff of Araeosia. $staff is lowercase form of each name for matching against a user-typed input, while $staffranks contains the proper capitalization names for use in things such as name color in the MOTD.
-$staff = array('agentkid', 'jasonbbb711', 'anoki123', 'canadiancellist', 'bratboy44', 'veryredgir', 'akwest', 'drsilverbullet', 'turkeymilk');
+$staff = array('agentkid', 'jasonbbb711', 'anoki123', 'kidwulf', 'canadiancellist', 'bratboy44', 'veryredgir', 'akwest', 'drsilverbullet', 'turkeymilk');
 $staffranks = array(
-	"admin" => array('AgentKid', 'jasonbbb711', 'CanadianCellist', 'VeryRedGir'),
+	"admin" => array('AgentKid', 'jasonbbb711', 'CanadianCellist', 'VeryRedGir', 'anoki123', 'KidWulf'),
 	"moderator" => array('bratboy44', 'DDRRPP', 'Akwest', 'DrSilverbullet', 'turkeymilk')
 );
 ?>
