d71092335cf43e2e23dea79f4d03271db8b774a2
AgentKid <agentkid20@gmail.com>
2012-06-19 03:15:02 -0400

    Inter-server kicking!

---
+++ interserver/kick.php
@@ -0,0 +1,33 @@
+<?php
+include('includes/staff.php');
+include('includes/servers.php');
+include('includes/functions.php');
+include('includes/passwords.php');
+$name = $_POST['player'];
+$world = $_POST['playerWorld'];
+$server = $_GET['s'];
+$args = $_POST['args'];
+//Argument 0 is kick, Argument 1 is name, Arguments 2-∞ are the kick message
+if(!in_array(strtolower($name), $staff)){ die('§cYou don\'t have permission to use that command!'); }
+if(!isset($args[2])){
+	$msg = "Kicked from server by ".$name."!";
+	$kickee = $args[1];
+}else{
+	$msg = $args;
+	array_shift($msg);
+	$kickee = array_shift($msg);
+	$msg = implode(' ', $msg);
+}
+$kicked=0;
+foreach($servers as $server){
+	$JSONAPI = new JSONAPI($ips[$server], $ports['jsonapi'][$server], $passwords['jsonapi']['user'], $passwords['jsonapi']['password'], $passwords['jsonapi']['salt']);
+	$players = $JSONAPI->call('getPlayerNames', array());
+	$players = $players['success'];
+	if(in_array($kickee, $players)){
+		$JSONAPI->call('kickPlayer', array($kickee, $msg));
+		echo "Kicked ".$kickee." from Araeosia ".$server." with the message \"".$msg."\".\n";
+		$kicked=$kicked+1;
+	}
+}
+if($kicked==0){ echo "§cCould not find player ".$kickee."!"; }
+?>
