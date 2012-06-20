7f3e1bd3c2af556f2bf4310c72aa801c03bb86ad
AgentKid <agentkid20@gmail.com>
2012-06-19 22:04:51 -0400

    Ban checks with permissions! :D

--- bancheck.php
+++ bancheck.php
@@ -7,13 +7,14 @@ $lookUp = $args[1];
 include('includes/staff.php');
 include('includes/mysql.php');
 include('includes/functions.php');
-
+if(!in_array(strtolower($name), $staff)){ die('§cYou do not have permission to perform this command!'); }
 // Fishbans! http://fishbans.com/
 $webLocation = "http://www.fishbans.com/api/bans/".$lookUp."/";
 $fileHandle = fopen($webLocation, 'r');
 $webOutput = fread($fileHandle, 1000000);
 $jsonDecoded = json_decode($webOutput, true);
 // Nice and simple.
+echo "§aLooking up §b".$lookUp."§a...\n";
 if(!$jsonDecoded['success']){ die('§c'.$jsonDecoded['error'].'!'); }
 $services = array_keys($jsonDecoded['bans']['service']);
 foreach($services as $service){
@@ -23,6 +24,8 @@ foreach($services as $service){
 		foreach($bankeys as $bankey){
 			echo "§b".trim($bankey)." §f- §4".trim($jsonDecoded['bans']['service'][$service]['ban_info'][$bankey])."\n";
 		}
+	}else{
+		echo "§b".$lookUp."§a has no bans on record from §b".$service."!\n";
 	}
 }
 ?>
