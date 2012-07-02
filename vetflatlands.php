4511a7158d9dfc75868c4bcf2a39f145a91171cd
AgentKid <agentkid20@gmail.com>
2012-07-02 18:52:05 -0400

    Should work now, but it's untested because of minecraft.net failing.

--- vetflatlands.php
+++ vetflatlands.php
@@ -2,4 +2,6 @@
 include('includes/mysql.php');
 include('includes/staff.php');
 include('includes/functions.php');
+if($_GET['s']!="Freebuild"){ exit; }
+if(isInGroup("Veteran", $_POST['player'])){ echo '/Command/ExecuteConsoleCommand:mvtp '.$_POST['player'].' VetFlatlands;\n§aYou were teleported to VetFlatlands.'}else{ echo "§cYou don't have permission to go to VetFlatlands!"; }
 ?>
