adc9f71ac4707b2663a80a168f3a481fbbe14e54
AgentKid <agentkid20@gmail.com>
2012-07-02 18:45:49 -0400

    Changes.

--- 2/chunk.php
+++ 2/chunk.php
@@ -1,8 +1,43 @@
 <?php
+set_time_limit(0);
 $name = "AgentKid";
-$range = array('start' => 4000, 'end' => 5000);
-
-while($range['start']>$range['end']){
-	
+$range = range(-500, 500, 1);
+array_splice($range, 251, 499);
+$range2 = range(-500, 500, 1);
+array_splice($range2, 251, 499);
+$chunksToMove = array();
+$count = 0;
+foreach($range as $value){
+	foreach($range2 as $value2){
+		$chunksToMove[$count] = array( 'X' => $value, 'Z' => $value2);
+		$count = $count+1;
+	}
 }
+error_reporting(E_ALL);
+/*
+ * We have $chunksToMove, which contains count($range)*count($range2) entries. Each in the form array('X' => int, 'Z' => int). Lets put them to work.
+ */
+$count = 0;
+var_dump(count($chunksToMove));
+foreach($chunksToMove as $chunk){
+	$count = $count+1;
+	$insideCoordinateX = $chunk['X']*16;
+	$insideCoordinateZ = $chunk['Z']*16;
+	$distanceFromOrigin = sqrt(pow($insideCoordinateX, 2)+pow($insideCoordinateZ, 2));
+	$reduced = abs(round($distanceFromOrigin/1000));
+	$shift = $reduced*3;
+#	if($count>8){ die('Killed.'); }
+/*
+ * We now know how much we're going to shift the chunk down by. Lets teleport the player to the sample spot.
+ */
+ 	echo "/Command/ExecuteConsoleCommand:mvtp AgentKid e:Araeosia2A:".$insideCoordinateX.",128,".$insideCoordinateZ.";\n";
+ 	echo "/Command/ExecuteBukkitCommand://chunk;\n";
+ 	echo "/Command/ExecuteBukkitCommand://move ".$shift." down;\n";
+ 	echo "/Command/ExecuteBukkitCommand://contract 255 down;\n";
+ 	echo "/Command/ExecuteBukkitCommand://set 7;\n";
+ 	echo "§aMoved chunk ".$chunk['X'].", ".$chunk['Z']." down by ".$shift." blocks.\n";
+ 	sleep(1);
+}
+
+
 ?>
