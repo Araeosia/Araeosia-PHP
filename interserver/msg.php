77911410f1c15dc7ba3b91813a3398d66f320639
AgentKid <agentkid20@gmail.com>
2012-07-04 22:39:54 -0400

    Testing new git location, added includes to msg.php

--- interserver/msg.php
+++ interserver/msg.php
@@ -2,5 +2,10 @@
 $name = $_POST['player'];
 $serversending = $_GET['s'];
 $args = $_POST['args'];
+include('includes/mysql.php');
+include('includes/servers.php');
+include('includes/functions.php');
+
+// Beginning of actual code
 
 ?>
