a44f988e5245639ab39520623c035f7b8221e3a7
AgentKid <agentkid20@gmail.com>
2012-07-06 15:03:52 -0400

    Object Oriented

--- who.php
+++ who.php
@@ -7,22 +7,12 @@ include('includes/functions.php');
 include('includes/servers.php');
 include('includes/staff.php');
 foreach($servers as $server){
-	$Query = new MinecraftQuery();
-	try{
-		$Query->Connect( $ips[$server], $ports['mc'][$server], 1 );
-		$players = $Query->GetPlayers();
-	}catch(MinecraftQueryException $e){
-		// Only reason this query would fail is if the server is down. Report it
-		$players = array();
-	}
+	$players = getOnlinePlayers($server);
+	$playersfinal = array();
 	// Now we have a populated (or empty) array of players, lets handle it.
-		$playersfinal = array();
-		foreach($players as $player){
-			if(in_array($player,$staffranks['admin'])){$player="§4".$player;}elseif(in_array($player,$staffranks['moderator'])){$player = "§a".$player;}else{$player = "§b".$player; }
-			array_push($playersfinal, $player);
-		}
-		$output = $output."§c------- §bAraeosia ".$server." §c-------\n";
-	if(count($playersfinal)!=0 && $playersfinal!=false){
+	foreach($players as $player){ array_push($playersfinal, getFullName($player)); }
+	$output = $output."§c------- §bAraeosia ".$server." §c-------\n";
+	if(count($playersfinal)!=0){
 		$output = $output."§aOnline§f: ".implode('§f, ', $playersfinal)."\n";
 	}else{
 		$output = $output."No online players!\n";
