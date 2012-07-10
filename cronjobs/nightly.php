c24f040a1420f50b76f99daf4d25bfbfc5a4614e
AgentKid <agentkid20@gmail.com>
2012-07-09 22:13:48 -0400

    Added chat log archiving.

--- cronjobs/nightly.php
+++ cronjobs/nightly.php
@@ -4,6 +4,13 @@
 // SQL Backups
 $loc = "/home/agentkid/backups";
 $time = time();
-$cmd = "mysqldump -u website -PASS password Araeosia > ".$loc.$time.".sql";
+$cmd = "mysqldump -u ".$passwords['mysql']['user']." -PASS ".$passwords['mysql']['pass']." Araeosia > ".$loc.$time.".sql";
 exec($cmd);
+
+// Archive the chat log
+$loc = "/home/agentkid/logs/chat.log";
+$date = date("m.d.y");
+$newloc = "/home/agentkid/logs/archive/chat.".$date.".log";
+rename($loc, $newloc);
+touch($loc);
 ?>
