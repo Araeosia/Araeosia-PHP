4752d5e6123b0197f5ca32fc14a313555ff8b5cb
AgentKid <agentkid20@gmail.com>
2012-07-21 06:34:11 -0700

    Added includes.php that includes all includes. Slightly heavier performance wise, but it cleans up code.

---
+++ includes/includes.php
@@ -0,0 +1,12 @@
+<?php
+// Because this is simpler :D
+include('includes/staff.php');
+include('includes/servers.php');
+include('includes/quests.php');
+include('includes/passwords.php');
+include('includes/mysql.php');
+include('includes/functions.php');
+include('includes/channels.php');
+include('includes/books.php');
+include('includes/blocks.php');
+?>
