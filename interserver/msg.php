89b63a569c8a5eaf5350b674f4dfcf06dd2dcbe2
AgentKid <agentkid20@gmail.com>
2012-07-27 17:25:30 -0400

    Now uses tellPlayer(). Beginning of /r.

--- interserver/msg.php
+++ interserver/msg.php
@@ -19,15 +19,9 @@ if(player($recipient)==false){ $recipient = $previousSender['recipient']; }
 if(player($recipient)==false){ die("§cCould not find a player by that name!"); }
 $finalMessageToRecipient = "§8(".getFullName($name)." §7to you§8)§f: ".$msg;
 $finalMessageToSender = "§8(§7you to ".getFullName(player($recipient))."§8)§f: ".$msg;
-
 $log = $timestamp."(".$name." to ".player($recipient)."): ".$msg."\n";
-
 echo $finalMessageToSender;
-
-foreach(getServersByPlayer(player($recipient)) as $server){
-	$JSONAPI = new JSONAPI($ips[$server], $ports['jsonapi'][$server], $passwords['jsonapi']['user'], $passwords['jsonapi']['password'], $passwords['jsonapi']['salt']);
-	$JSONAPI->call('sendMessage', array(player($recipient), $finalMessageToRecipient));
-}
+tellPlayer($recipient, $finalMessageToRecipient);
 $logfile = fopen('/home/agentkid/logs/chat.log', 'a');
 fwrite($logfile, str_replace(array('§1', '§2', '§3', '§4', '§5', '§6', '§7', '§8', '§9', '§0', '§a', '§b', '§c', '§d', '§e', '§f'), '', $log));
 fclose($logfile);
