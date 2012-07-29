3df0392967b27bb3727a0e5277f4f02713ed919c
AgentKid <agentkid20@gmail.com>
2012-07-29 19:52:47 -0400

    Uses ChannelHandle to get variables, clears screen, added new player handling.

--- othermotd.php
+++ othermotd.php
@@ -18,29 +18,35 @@ switch($world){
 		$worldname = $world;
 		break;
 }
-if(mysql_fetch_array(mysql_query("SELECT * FROM ChannelsIn WHERE name='$name'"))===false){
-	mysql_query("INSERT INTO ChannelsIn (id, name, channel, type) VALUES ('NULL', '$name', 'A', '1')");
-	echo "§aYou joined the §eAraeosia §achannel!\n";
-}
+$chatHandle = new ChannelHandle($name);
 if(mysql_fetch_array(mysql_query("SELECT * FROM TrueGroups WHERE name='$name'"))==false){
+	$newUser = true;
 	mysql_query("INSERT INTO TrueGroups VALUES ('NULL', '$name', 'Default')") or die(mysql_error());
-}
+}else{ $newUser = false; }
+// New code
+if($newUser){
+	sysMessage("§6Everyone welcome ".getFullName($name)."§6 to the server for the first time!");
+	
+}else{
+
 // Go ahead and cache the user in FishBans, in case we want to look them up later.
-#$fishBans = new FishBans();
-#$fishBans->isCached($name);
+#	$fishBans = new FishBans();
+#	$fishBans->isCached($name);
+	$online = getAllPlayers();
+	if(!in_array($name, $online)){ array_push($online, $name); }
+	$online = rankPlayers($online);
+	$onlinect = count($online);
+	$online = implode($online, '§e, ');
 
-$online = rankPlayers(getAllPlayers());
-$onlinect = count($online);
-$online = implode($online, '§e, ');
-
-$msg = "§bWelcome to the Araeosia Freebuild Server, ".$name."!\n";
-$msg = $msg."§3You are currently in ".getWorldName($worldname)."§3.\n";
-$msg = $msg."§eOnline (".$onlinect."/512): §b".$online."§e.\n";
-$msg = $msg."§cNews: §e/news\n";
-$msg = $msg."§4Araeosia has adopted a No-Mercy policy. §e/nomercy§4 for info.\n";
-$msg = $msg."§4Rules: §e/rules - Updated 2-4-2012\n";
-if(date("j F Y")=="4 July 2012"){ $msg = $msg."§fHappy §3Fourth of §4July!"; }
-if(date("j F Y")=="25 July 2012"){ $msg = $msg."§aA new Survival world has been created!\n"; }
-#$msg = $msg."§4AgentKid §ahas returned from vacation!";
-echo $msg;
+	clearScreen();
+	$msg = "§bWelcome to the Araeosia Freebuild Server, ".$name."!\n";
+	$msg = $msg."§3You are currently in ".getWorldName($worldname)."§3.\n";
+	$msg = $msg."§eOnline (".$onlinect."/512): §b".$online."§e.\n";
+	$msg = $msg."§cNews: §e/news\n";
+	$msg = $msg."§4Araeosia has adopted a No-Mercy policy. §e/nomercy§4 for info.\n";
+	$msg = $msg."§4Rules: §e/rules - Updated 2-4-2012\n";
+	if(date("j F Y")=="4 July 2012"){ $msg = $msg."§fHappy §3Fourth of §4July!"; }
+#	$msg = $msg."§4AgentKid §ahas returned from vacation!";
+	echo $msg;
+}
 ?>
