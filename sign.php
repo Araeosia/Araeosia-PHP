163dee3cdf4ff03b3a0f687814ced40d591be826
AgentKid <agentkid20@gmail.com>
2012-07-29 19:53:33 -0400

    Added sign handling.

---
+++ sign.php
@@ -0,0 +1,9 @@
+<?php
+$name = $_POST['player'];
+switch(true){
+	case $X=766 && $Y=68 && $Z=636 && $W='Araeosia':
+		echo "§4=============== Important Announcement ===============\nDevelopment of Araeosia 1 has been discontinued. We originally intended Araeosia to be a map that would be run by an admin for a one night event, with the admins manually controlling that. When we realized how large the project was becoming, we decided to move to an NPC based system with quests, but that turned out to be too complex to manage, with us lacking much needed features from both SpoutCraft and Citizens. Because of this, we have decided to terminate the development of Araeosia 1 and move on to our new project, Araeosia 2. \nIn the meantime, though, we'll leave this world open for everyone to explore. There are some semi-completed quests that you can attempt to do, but for the most part you'll just be exploring.\n\n§aSigned, Araeosia Staff.";
+		break;
+}
+
+?>
