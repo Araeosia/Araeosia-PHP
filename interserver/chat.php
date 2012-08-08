a43789eb2445812745fab4611bd75e848e80defb
AgentKid <agentkid20@gmail.com>
2012-08-08 11:35:45 -0400

    Removed old Modded code, flush()

--- interserver/chat.php
+++ interserver/chat.php
@@ -1,7 +1,6 @@
 <?php
 $name = $_POST['player'];
 $world = $_POST['playerWorld'];
-$serversending = $_GET['s'];
 $msg = $_POST['args'];
 array_shift($args);
 include('includes/servers.php');
@@ -15,20 +14,8 @@ $msg = str_replace('    ', '', $msg);
 $channelHandle = new ChannelHandle($name);
 $channel = $channelHandle->currentChannel;
 $log = "[".date('m-d-y H:i:s', time())."] ".getChannelColor($channel)."[".$channel."]"." §f[§9".getWorldName($world)."§f] ".getFullName($name)."§f: ".$msg."\n";
-if($serversending=="Modded" && strpos($msg, "echo982")!==false){
-	$JSONAPI = new JSONAPI($ips["Modded"], $ports['jsonapi']["Modded"], $passwords['jsonapi']['user'], $passwords['jsonapi']['password'], $passwords['jsonapi']['salt']);
-	$JSONAPI->call('setBlockType', array('Industry', 2016, 62, 32, 2));
-	$JSONAPI->call('setBlockType', array('Industry', 2016, 62, 33, 2));
-	$JSONAPI->call('setBlockType', array('Industry', 2016, 62, 34, 2));
-	sleep(10);
-	$JSONAPI->call('setBlockType', array('Industry', 2016, 62, 32, 76));
-	$JSONAPI->call('setBlockType', array('Industry', 2016, 62, 33, 76));
-	$JSONAPI->call('setBlockType', array('Industry', 2016, 62, 34, 76));
-	exit;
-}
 if($channelHandle->isMute($channel) || $channelHandle->isMute()){ die('§cYou are currently muted!'); }
 echo formatOutput($channel, $name, $msg, $world, $channelHandle->style);
-flush();
 sendMessageToChannel($channel, $name, $msg, $world, array($name));
 $logfile = fopen('/home/agentkid/logs/chat.log', 'a');
 fwrite($logfile, str_replace(array('§1', '§2', '§3', '§4', '§5', '§6', '§7', '§8', '§9', '§0', '§a', '§b', '§c', '§d', '§e', '§f'), '', $log));
